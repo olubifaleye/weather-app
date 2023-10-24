07ec48b7305a15323f7468330285ef4d93c5f206
olubifaleye <eniolubiportfolio@gmail.com>
2023-10-24 18:47:07 +0100

    Weather App multiple file updates and a file removal
    formValidation.js: removed the JS file
    
    views.py: Added status code validation for POST requests alongside importing messages to implement toast messages.
    
    index.html: added html content for displaying toasts, and content to allow Materialize CSS Styling
    
    city_weather.html: Changed the h tags
    
    Added 2 materialize.min files; css and js files.
    
    globals.css: Updated CSS

--- weather_project/weather_app/static/formValidation.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-
-    validCities = []
-
-
-    let city1 = document.forms["cityForm"]["city1"].value;
-    let city2 = document.forms["cityForm"]["city2"].value;
-}
--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 import requests
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 
@@ -30,16 +31,16 @@ def index(request):
 
 
         # Fetch information from the forecast and render into the data template
-        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_weather_url)
+        weather_data1, daily_forecasts1, code1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_weather_url, request)
 
         # Check if there is a second city
         if city2:
 
             # Create fetch variables for forecast information
-            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forecast_weather_url)
+            weather_data2, daily_forecasts2, code2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forecast_weather_url, request)
         else:
             # If not set to None
-            weather_data2, daily_forecasts2 = None, None
+            weather_data2, daily_forecasts2, code2 = None, None, 404
 
         # Context dictionary
         context = {
@@ -47,25 +48,30 @@ def index(request):
             "daily_forecasts1": daily_forecasts1,
             "weather_data2": weather_data2,
             "daily_forecasts2": daily_forecasts2,
+            "code1": code1,
+            "code2": code2,
         }
 
         # render a html template
         return render(request, "weather_app/index.html", context)
 
     else:
+
         # render a html template
         return render(request, "weather_app/index.html")
     
 
-
 # Fetch weather and forecast
-def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_weather_url):
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_weather_url, request):
 
     # send GET request to the current forecast url 
     # take city and API Key as format in JSON
     response = requests.get(current_weather_url.format(city, api_key)).json()
 
-    if response['cod'] == 200:
+    # Set code variable to the response code 
+    code = response['cod']
+
+    if code == 200:
 
         # Extract Long and Lat Coordinates
         lat = response['coord']['lat']
@@ -106,13 +112,19 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_weat
                 "icon": data['weather'][0]['icon'] 
             })
 
+        # Success message for toasts
+        messages.success(request, "City Found")
+
         # return weather data and daily forecast data collected
-        return weather_data, daily_forecasts
+        return weather_data, daily_forecasts, code
     
     else:
         # Set the data collections to be empty as the city doesn't exist
-        weather_data = {}
-        daily_forecasts = []
+        weather_data = None
+        daily_forecasts = None
+
+        # Error message for toast
+        messages.error(request, "No City found. Try again!")
 
         # return weather data and daily forecast 
-        return weather_data, daily_forecasts;
+        return weather_data, daily_forecasts, code
