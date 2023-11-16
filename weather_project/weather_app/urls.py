ecab721a5dac85bc39e30f33f101f571dd38be76
olubifaleye <eniolubiportfolio@gmail.com>
2023-11-16 23:44:23 +0000

    Update urls.py

--- weather_project/weather_app/urls.py
+++ weather_project/weather_app/urls.py
@@ -2,8 +2,13 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 # Set URL patterns 
 urlpatterns = [
     path("", views.index, name="index")
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
